dea87262c2df7fa018067b3a90beb8063d3f5559
Isabella Campos <isabellaszcampos@gmail.com>
2021-12-02 13:55:51 -0300

    Add files via upload

---
+++ js/animacao.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: "bottom",
+    distance: "20px",
+    duration: 1000,
+  });
+  
+  ScrollReveal().reveal(".titulo", { delay: 300 });
+  ScrollReveal().reveal(".container",  { delay: 400 });
+  ScrollReveal().reveal(".container-solucao",  { delay: 400 });
+  
